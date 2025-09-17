5268af679142e497733ba6d0649df88551b3041f
Fen Dweller <chemicalcrux@gmail.com>
2025-09-17 12:11:50 -0500

    Add a script to reserialize assets
    This makes sure all serialized data is upgraded to the latest possible
    version, rather than just waiting for it to happen to be reserialized.

---
+++ Packages/best.crux.core/Developer/ReserializeAssets.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using Crux.Core.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+public static class ReserializeAssets
+{
+    [MenuItem("Assets/Reserialize", false, CoreConsts.ContextAssetOrder)]
+    static void Reserialize()
+    {
+        var path = AssetDatabase.GetAssetPath(Selection.activeObject);
+
+        Debug.Log(path);
+        if (AssetDatabase.TryGetGUIDAndLocalFileIdentifier(Selection.activeObject, out var guid, out long fileID))
+        {
+            var targets = AssetDatabase.FindAssets("", new[] { path })
+                         .Select(static guid => AssetDatabase.GUIDToAssetPath(guid));
+            
+            AssetDatabase.ForceReserializeAssets(targets);
+        }
+    }
+
+    [MenuItem("Assets/Reserialize", true, CoreConsts.ContextAssetOrder)]
+    static bool ReserializeValidation()
+    {
+        return Selection.activeObject && AssetDatabase.Contains(Selection.activeObject) &&
+               Selection.activeObject is DefaultAsset;
+    }
+}
